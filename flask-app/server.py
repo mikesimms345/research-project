a7f17be3643240fe35a6f8cbd08ac0604e883d6e
mikesimms345 <mikespike137@gmail.com>
2025-06-30 12:28:26 -0400

    Fixed logic, created random room keys

--- flask-app/server.py
+++ flask-app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request,  render_template
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__, static_folder='static')
@@ -12,33 +12,33 @@ def index():
 def handle_join(data):
     room = data.get('room')
     join_room(room)
-    emit('joined', {'sid': request.sid}, to=room)
-    print(f"[JOIN] {request.sid} joined room {room}")
+    emit('joined', {'sid': request.sid}, to=room, include_self=False)
+    print(f"(Join) {request.sid} joined room {room}")
 
 @socketio.on('offer')
 def handle_offer(data):
     room = data.get('room')
     offer = data.get('offer')
     emit('offer', offer, to=room, include_self=False)
-    print(f"[OFFER] Sent offer from {request.sid} to room {room}")
+    print(f"(Offer) Sent offer from {request.sid} to room {room}")
 
 @socketio.on('answer')
 def handle_answer(data):
     room = data.get('room')
     answer = data.get('answer')
     emit('answer', answer, to=room, include_self=False)
-    print(f"[ANSWER] Sent answer from {request.sid} to room {room}")
+    print(f"(Answer) Sent answer from {request.sid} to room {room}")
 
 @socketio.on('ice-candidate')
 def handle_ice_candidate(data):
     room = data.get('room')
     candidate = data.get('candidate')
     emit('ice-candidate', candidate, to=room, include_self=False)
-    print(f'[ICE] Sent candidate from {request.sid} to room {room}')
+    print(f'(ICE) Sent candidate from {request.sid} to room {room}')
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print(f"[DISCONNECT] {request.sid} disconnected")
+    print(f"(Disconnect) {request.sid} disconnected")
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8081, allow_unsafe_werkzeug=True, ssl_context=('cert.pem', 'key.pem'))
